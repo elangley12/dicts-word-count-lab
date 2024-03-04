c80d006c0d39507734b9413dc599c9c659844d81
Emily <emily.j.langley@gmail.com>
2024-03-04 12:23:26 -0800

    added key value argument

--- wordcount.py
+++ wordcount.py
@@ -5,11 +5,13 @@ def get_word_count(text_file):                                      # define fun
     text_file = open(text_file)                                     # open text file for iteration later
     word_counts = {}                                                # make an empty dictionary
     
-    for line in text_file:                                          # loop through text file word by word
+    for line in text_file:                                          # parse through each line of the text file
         words = line.rstrip().split()
-        for word in words:
-            word_counts[word] = word_counts.get(word, 0) + 1            # add one for each word to new dictionary
+        for word in words:                                          # loop through text file word by word
+            word_counts[word] = word_counts.get(word, 0) + 1        # add one for each word to new dictionary
+    for key, value in word_counts():
+        print(f"{key}, {value}")
 
-    return word_counts
+    # return word_counts
 
 
