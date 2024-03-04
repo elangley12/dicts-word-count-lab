761d943e3da34d390600ff2980dff756db2f8143
Emily <emily.j.langley@gmail.com>
2024-03-04 12:02:56 -0800

    added pseudocode

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,10 @@
 """Count words in file."""
 
+def get_word_count(text_file):          # define function that takes text file as parameter
+    # make an empty dictionary
+    # loop through text file word by word
+        # add one for each word to new dictionary
 
-# put your code here.
+    # return dictionary
+
+
