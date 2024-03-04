63e9bc679eff69c14401686e8f00472cb33c570f
Emily <emily.j.langley@gmail.com>
2024-03-04 12:06:19 -0800

    first pass at code

--- wordcount.py
+++ wordcount.py
@@ -1,10 +1,11 @@
 """Count words in file."""
 
-def get_word_count(text_file):          # define function that takes text file as parameter
-    # make an empty dictionary
-    # loop through text file word by word
-        # add one for each word to new dictionary
+def get_word_count(text_file):                                      # define function that takes text file as parameter
+    word_counts = {}                                                # make an empty dictionary
+    
+    for word in text_file:                                          # loop through text file word by word
+        word_counts[word] = word_counts.get(word, 0) + 1            # add one for each word to new dictionary
 
-    # return dictionary
+    return word_counts
 
 
